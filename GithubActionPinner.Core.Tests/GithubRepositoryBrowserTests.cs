386ad87d73465d0dadf0b64648e73a66bc6273b0
Marc Stan <marcstan@live.com>
2020-04-12 11:56:31 +0200

    check rate limits during test and issue build warnings

--- GithubActionPinner.Core.Tests/GithubRepositoryBrowserTests.cs
+++ GithubActionPinner.Core.Tests/GithubRepositoryBrowserTests.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,6 +16,16 @@ namespace GithubActionPinner.Core.Tests
         /// https://help.github.com/en/actions/configuring-and-managing-workflows/authenticating-with-the-github_token
         /// </summary>
         private static readonly string _token = Environment.GetEnvironmentVariable("GITHUB_TOKEN");
+        private static readonly HttpClient _httpClient;
+
+        static GithubRepositoryBrowserTests()
+        {
+            _httpClient = new HttpClient();
+            // https://developer.github.com/v3/#current-version
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            // https://developer.github.com/v3/#user-agent-required
+            _httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("MarcStan%2Fgithub-action-pinner%2Fintegration-tests", "v1"));
+        }
 
         [DataTestMethod]
         [DataRow("MarcStan", "github-action-pinner", true)]
@@ -31,11 +45,42 @@ namespace GithubActionPinner.Core.Tests
             try
             {
                 await task();
+
+                // does not count against rate limit
+                var response = await _httpClient.GetAsync("https://api.github.com/rate_limit");
+                response.EnsureSuccessStatusCode();
+                var obj = await JsonSerializer.DeserializeAsync<RatelimitResponse>(await response.Content.ReadAsStreamAsync());
+                var c = obj.Resources.Core;
+                Console.WriteLine($"WARNING: {c.Remaining}/{c.Limit} api calls remaining (resets {c.Reset}). If you have been running the tests a lot, expect them to start failing soon.");
             }
             catch (GithubApiRatelimitExceededException ex)
             {
                 Assert.Fail(ex.Message +
                     "If you are running locally, consider setting a personal access token with `repo` permissions as `GITHUB_TOKEN` environment variable");
+            }
+        }
+
+        private class RatelimitResponse
+        {
+            [JsonPropertyName("resources")]
+            public ResourcesObject Resources { get; set; } = new ResourcesObject();
+
+            public class ResourcesObject
+            {
+                [JsonPropertyName("core")]
+                public RatelimitObject Core { get; set; } = new RatelimitObject();
+            }
+
+            public class RatelimitObject
+            {
+                [JsonPropertyName("limit")]
+                public int Limit { get; set; }
+
+                [JsonPropertyName("remaining")]
+                public int Remaining { get; set; }
+
+                [JsonPropertyName("reset")]
+                public long Reset { get; set; }
             }
         }
     }
