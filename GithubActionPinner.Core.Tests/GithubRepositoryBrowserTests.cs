decde667ced4750e14518cbe0da932826295a815
Marc Stan <marcstan@live.com>
2020-04-12 12:40:45 +0200

    ignore rate limit and - error message with guidance is printed on failure

--- GithubActionPinner.Core.Tests/GithubRepositoryBrowserTests.cs
+++ GithubActionPinner.Core.Tests/GithubRepositoryBrowserTests.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -16,16 +12,6 @@ namespace GithubActionPinner.Core.Tests
         /// https://help.github.com/en/actions/configuring-and-managing-workflows/authenticating-with-the-github_token
         /// </summary>
         private static readonly string _token = Environment.GetEnvironmentVariable("GITHUB_TOKEN");
-        private static readonly HttpClient _httpClient;
-
-        static GithubRepositoryBrowserTests()
-        {
-            _httpClient = new HttpClient();
-            // https://developer.github.com/v3/#current-version
-            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-            // https://developer.github.com/v3/#user-agent-required
-            _httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("MarcStan%2Fgithub-action-pinner%2Fintegration-tests", "v1"));
-        }
 
         [DataTestMethod]
         [DataRow("MarcStan", "github-action-pinner", true)]
@@ -48,46 +34,8 @@ namespace GithubActionPinner.Core.Tests
             }
             catch (GithubApiRatelimitExceededException ex)
             {
-                Assert.Fail(ex.Message +
-                    "If you are running locally, consider setting a personal access token with `repo` permissions as `GITHUB_TOKEN` environment variable");
-            }
-            finally
-            {
-                // does not count against rate limit so we can always run it (even if ratelimit has been hit already)
-                var response = await _httpClient.GetAsync("https://api.github.com/rate_limit");
-                response.EnsureSuccessStatusCode();
-                var obj = await JsonSerializer.DeserializeAsync<RatelimitResponse>(await response.Content.ReadAsStreamAsync());
-                var c = obj.Resources.Core;
-                // issue when getting close to rate limit
-                if (c.Remaining < 100 || c.Remaining < c.Limit / 10.0)
-                {
-                    // as per https://github.com/actions/toolkit/blob/1725272151f6cc845f4ae86a925c31860c2b7beb/packages/core/src/command.ts#L16
-                    Console.WriteLine($"##[warning]{c.Remaining}/{c.Limit} api calls remaining (resets {c.Reset}). If you have been running the tests a lot, expect them to start failing soon.");
-                }
-            }
-        }
-
-        private class RatelimitResponse
-        {
-            [JsonPropertyName("resources")]
-            public ResourcesObject Resources { get; set; } = new ResourcesObject();
-
-            public class ResourcesObject
-            {
-                [JsonPropertyName("core")]
-                public RatelimitObject Core { get; set; } = new RatelimitObject();
-            }
-
-            public class RatelimitObject
-            {
-                [JsonPropertyName("limit")]
-                public int Limit { get; set; }
-
-                [JsonPropertyName("remaining")]
-                public int Remaining { get; set; }
-
-                [JsonPropertyName("reset")]
-                public long Reset { get; set; }
+                Assert.Fail(ex.Message + Environment.NewLine +
+                    "If you are running locally, consider setting a personal access token with `repo` permissions as `GITHUB_TOKEN` environment variable.");
             }
         }
     }
